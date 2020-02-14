afda1cceaa05c2e86c2eb8906797c0ad4ddb33cb
d0tc0m-exe <vey.the.user@gmail.com>
2020-02-14 14:32:31 +0200

    mutable maps, lists

--- src/functions.kt
+++ src/functions.kt
@@ -7,9 +7,15 @@
 fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGreet") 
 
 fun main() {
-    val things = listOf("Kotlin",  "Coding")
+//    val things = listOf("Kotlin",  "Coding")
+    val things = mutableListOf("Kotlin",  "Coding")
+    things.add("Tutorials")
 
-    val map = mapOf(1 to "K", 2 to "O", 3 to "T", 4 to "L", 5 to "I", 6 to "N")
+//    val map = mapOf(1 to "K", 2 to "O", 3 to "T", 4 to "L", 5 to "I", 6 to "N")
+    val map = mutableMapOf(1 to "K", 2 to "O", 3 to "T", 4 to "L", 5 to "I", 6 to "N")
+    map.put(7, "T")
+    map.put(8, "O")
+    map.put(9, "P")
 
     map.forEach {key, value -> println("$key -> $value")}
 //    things.forEach { things ->
